a2808d7565235ff008d4f82600e942595354446a
Boris Ceranic <me@sosko.in.rs>
2015-11-24 00:33:43 +0100

    Replace const with var (ES5 compatibility)

--- index.js
+++ index.js
@@ -1,10 +1,10 @@
-const Random = require('random-js')
+var Random = require('random-js')
 
-const stringLength = 16
+var stringLength = 16
 
-const uniqueId = function() {
-    const randomEngine = Random.engines.mt19937().autoSeed();
-    const randomDistrib = Random.string();
+var uniqueId = function() {
+    var randomEngine = Random.engines.mt19937().autoSeed();
+    var randomDistrib = Random.string();
 
     return randomDistrib(randomEngine, stringLength)
 }
